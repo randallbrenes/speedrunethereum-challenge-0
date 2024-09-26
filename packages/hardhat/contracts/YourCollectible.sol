dec563f07aef85710d7331e1d6b3d89465bf0e73
Randall Brenes <brenes.randall@gmail.com>
2024-09-26 14:31:40 -0600

    Initial

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2; //Do not change the solidity version as it negatively impacts submission grading
+pragma solidity ^0.8.27; //Do not change the solidity version as it negatively impacts submission grading
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
